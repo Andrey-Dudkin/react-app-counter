1bb723cf382aeb84e1ed7dfe0680f3fbbe0aebdf
Andrey-Dudkin <a.dudkin.front@gmail.com>
2022-08-26 19:53:17 +0300

    react app countre temperatura

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -2,17 +2,66 @@ import React, { useState } from 'react'
 import "./Counter.css"
 
 const Counter = () => {
-  const [counter,setCounter] = useState(0)
-  // if(setCounter(counter <= 0)){
-  //   return <button className='btn_counter' disabled={true}>-</button>
-  // }
+  const [temperature,setTemperature] = useState(10)
+  const [color,setColor] = useState("chilly")
+
+  function plusTemperature(){
+    if(temperature === 45) return;
+
+    const newTemperature = temperature + 1;
+    if(newTemperature >= -15){
+      setColor("very_cold")
+    }
+    if(newTemperature >= -10){
+      setColor("cold")
+    }
+    if(newTemperature >= 0){
+      setColor("chilly")
+    }
+    if(newTemperature >= 20){
+      setColor("warm")
+    }
+    if(newTemperature >= 30){
+      setColor("hot")
+    }
+    setTemperature(newTemperature)
+  }
+
+  function minusTemperature(){
+    if(temperature === -35) return;
+
+    const newTemperature = temperature - 1;
+
+    if(newTemperature <= 30){
+      setColor("warm")
+    }
+    if(newTemperature <= 20){
+      setColor("chilly")
+    }
+    if(newTemperature <= 10){
+      setColor("cold")
+    }
+    if(newTemperature <= 0){
+      setColor("very_cold")
+    }
+    if(newTemperature <= -15){
+      setColor("crazy_cold")
+    }
+    setTemperature(newTemperature)
+  }
+  
   return (
     <div className='counter'>
-      <h1 className="title"> App counter</h1>
-      <h2 className="number_color">{counter}</h2>
-      <button className='btn_counter'onClick={() => {setCounter(counter +1)}}>+</button>
-      <button className='btn_counter'onClick={() => {setCounter(counter -1)}}>-</button>
-      <button className='btn_counter'onClick={() => {setCounter(0)}}>reset</button>
+      <h2 className="title">temperature degree counter</h2>
+      <div className="app-counter">
+          <div className="temperature-display-container">
+            <div className={`temperature-display ${color}`}>&#176;{temperature}С</div>
+          </div>
+          <div className="button-container">
+            <button onClick={() => plusTemperature()}>+</button>
+            <button onClick={() => minusTemperature()}>-</button>
+          </div>
+      </div>
     </div>
   )
  
